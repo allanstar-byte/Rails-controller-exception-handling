7f91249a9b29aa4d789973b6172ee9a5c00ee1a2
allanstar-byte <allanmutisya263@gmail.com>
2022-10-05 10:21:19 +0300

    Refactoring

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,87 @@
+# class BirdsController < ApplicationController
+#   # GET /birds
+#   def index
+#     birds = Bird.all
+#     render json: birds
+#   end
+
+#   # POST /birds
+#   def create
+#     bird = Bird.create(bird_params)
+#     render json: bird, status: :created
+#   end
+
+#   def show
+#     bird = find_bird
+#     if bird
+#       render json: bird
+#     else
+#       render_not_found_response
+#     end
+#   end
+  
+#   # PATCH /birds/:id
+#   def update
+#     bird = find_bird
+#     if bird
+#       bird.update(bird_params)
+#       render json: bird
+#     else
+#       render_not_found_response
+#     end
+#   end
+
+#   # PATCH /birds/:id/like
+#   def increment_likes
+#     bird = Bird.find_by(id: params[:id])
+#     if bird
+#       bird.update(likes: bird.likes + 1)
+#       render json: bird
+#     else
+#       # render json: { error: "Bird not found" }, status: :not_found
+#       render_not_found_response
+#     end
+#   end
+
+#   # DELETE /birds/:id
+#   def destroy
+#     bird = find_bird
+#     if bird
+#       bird.destroy
+#       head :no_content
+#     else
+#       # render json: { error: "Bird not found" }, status: :not_found
+#       render_not_found_response
+#     end
+#   end
+
+#   private
+
+#   def bird_params
+#     params.permit(:name, :species, :likes)
+#   end
+
+#   # renering error helper
+#   private
+
+#   def render_not_found_response
+#     render json: { error: "Bird not found" }, status: :not_found
+#   end
+#   private
+
+#     # finding helper
+#   def find_bird
+#     Bird.find_by(id: params[:id])
+#   end
+
+# end
+
+
+
+# Fully refacored code
+
 class BirdsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /birds
   def index
@@ -14,51 +97,43 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    render json: bird
   end
 
   # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(bird_params)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(bird_params)
+    render json: bird
   end
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(likes: bird.likes + 1)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
   end
 
   # DELETE /birds/:id
   def destroy
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.destroy
-      head :no_content
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.destroy
+    head :no_content
   end
 
   private
 
+  def find_bird
+    Bird.find(params[:id])
+  end
+
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 
 end
